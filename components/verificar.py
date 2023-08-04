40479a8f3bfedee4b54bfe1d1af8e787bcf7aa86
Leandro Cavalcante <devleandromcz@gmail.com>
2023-08-03 23:55:27 -0300

    update

--- components/verificar.py
+++ components/verificar.py
@@ -7,24 +7,95 @@ def cheia(matriz):
     return True
 
 def verificar(matriz):
+
+    #Azul ou vermelha
     horizontal = 0
     while horizontal < 4:
-        if 'A' in (matriz[horizontal][0], matriz[horizontal][1], matriz[horizontal][2], matriz[horizontal][3]):
+        if 'A' in matriz[horizontal][0] and 'A' in matriz[horizontal][1] and 'A' in matriz[horizontal][2] and 'A' in matriz[horizontal][3]:
             return 'A'
-        if 'V' in (matriz[horizontal][0], matriz[horizontal][1], matriz[horizontal][2], matriz[horizontal][3]):
+        if 'V' in matriz[horizontal][0] and 'V' in matriz[horizontal][1] and 'V' in matriz[horizontal][2] and 'V' in matriz[horizontal][3]:
             return 'V'
         horizontal += 1
     
     vertical = 0
     while vertical < 4:
-        if 'A' in (matriz[0][vertical], matriz[1][vertical], matriz[2][vertical], matriz[3][vertical]):
+        if 'A' in matriz[0][vertical] and 'A' in matriz[1][vertical] and 'A' in matriz[2][vertical] and 'A' in matriz[3][vertical]:
             return 'A'
-        if 'V' in (matriz[0][vertical], matriz[1][vertical], matriz[2][vertical], matriz[3][vertical]):
+        if 'V' in matriz[0][vertical] and 'V' in matriz[1][vertical] and 'V' in  matriz[2][vertical] and 'V' in matriz[3][vertical]:
             return 'V'
         vertical += 1
     
-    if 'A' in (matriz[0][0], matriz[1][1], matriz[2][2], matriz[3][3]):
+    if 'A' in matriz[0][0] and 'A' in matriz[1][1] and 'A' in matriz[2][2] and 'A' in matriz[3][3]:
         return 'A'
-    if 'V' in (matriz[0][0], matriz[1][1], matriz[2][2], matriz[3][3]):
+    if 'V' in matriz[0][0] and 'V' in matriz[1][1] and 'V' in matriz[2][2] and 'V' in matriz[3][3]:
         return 'V'
+    
+    #Quadrado ou redondo
+    horizontal = 0
+    while horizontal < 4:
+        if 'Q' in matriz[horizontal][0] and 'Q' in matriz[horizontal][1] and 'Q' in matriz[horizontal][2] and 'Q' in matriz[horizontal][3]:
+            return 'Q'
+        if 'R' in matriz[horizontal][0] and 'R' in matriz[horizontal][1] and 'R' in matriz[horizontal][2] and 'R' in matriz[horizontal][3]:
+            return 'R'
+        horizontal += 1
+    
+    vertical = 0
+    while vertical < 4:
+        if 'Q' in matriz[0][vertical] and 'Q' in matriz[1][vertical] and 'Q' in matriz[2][vertical] and 'Q' in matriz[3][vertical]:
+            return 'Q'
+        if 'R' in matriz[0][vertical] and 'R' in matriz[1][vertical] and 'R' in  matriz[2][vertical] and 'R' in matriz[3][vertical]:
+            return 'R'
+        vertical += 1
+    
+    if 'Q' in matriz[0][0] and 'Q' in matriz[1][1] and 'Q' in matriz[2][2] and 'Q' in matriz[3][3]:
+        return 'Q'
+    if 'R' in matriz[0][0] and 'R' in matriz[1][1] and 'R' in matriz[2][2] and 'R' in matriz[3][3]:
+        return 'R'
 
+    #Grande ou pequena
+    horizontal = 0
+    while horizontal < 4:
+        if 'G' in matriz[horizontal][0] and 'G' in matriz[horizontal][1] and 'G' in matriz[horizontal][2] and 'G' in matriz[horizontal][3]:
+            return 'G'
+        if 'P' in matriz[horizontal][0] and 'P' in matriz[horizontal][1] and 'P' in matriz[horizontal][2] and 'P' in matriz[horizontal][3]:
+            return 'P'
+        horizontal += 1
+    
+    vertical = 0
+    while vertical < 4:
+        if 'G' in matriz[0][vertical] and 'G' in matriz[1][vertical] and 'G' in matriz[2][vertical] and 'G' in matriz[3][vertical]:
+            return 'G'
+        if 'P' in matriz[0][vertical] and 'P' in matriz[1][vertical] and 'P' in  matriz[2][vertical] and 'P' in matriz[3][vertical]:
+            return 'P'
+        vertical += 1
+    
+    if 'G' in matriz[0][0] and 'G' in matriz[1][1] and 'G' in matriz[2][2] and 'G' in matriz[3][3]:
+        return 'G'
+    if 'P' in matriz[0][0] and 'P' in matriz[1][1] and 'P' in matriz[2][2] and 'P' in matriz[3][3]:
+        return 'P'
+
+    #Furada ou não furada
+    horizontal = 0
+    while horizontal < 4:
+        if 'F' in matriz[horizontal][0] and 'F' in matriz[horizontal][1] and 'F' in matriz[horizontal][2] and 'F' in matriz[horizontal][3]:
+            return 'F'
+        if 'N' in matriz[horizontal][0] and 'N' in matriz[horizontal][1] and 'N' in matriz[horizontal][2] and 'N' in matriz[horizontal][3]:
+            return 'N'
+        horizontal += 1
+    
+    vertical = 0
+    while vertical < 4:
+        if 'F' in matriz[0][vertical] and 'F' in matriz[1][vertical] and 'F' in matriz[2][vertical] and 'F' in matriz[3][vertical]:
+            return 'F'
+        if 'N' in matriz[0][vertical] and 'N' in matriz[1][vertical] and 'N' in  matriz[2][vertical] and 'N' in matriz[3][vertical]:
+            return 'N'
+        vertical += 1
+    
+    if 'F' in matriz[0][0] and 'F' in matriz[1][1] and 'F' in matriz[2][2] and 'F' in matriz[3][3]:
+        return 'F'
+    if 'N' in matriz[0][0] and 'N' in matriz[1][1] and 'N' in matriz[2][2] and 'N' in matriz[3][3]:
+        return 'N'
+
+    if cheia(matriz):
+        return 'Deu velha!'
+    return True
--- quarto.py
+++ quarto.py
@@ -20,27 +20,39 @@ def colorir(var):
 def print_game(matriz):
     print(f'\n----------------------------\n {colorir(matriz[0][0])} | {colorir(matriz[0][1])} | {colorir(matriz[0][2])} | {colorir(matriz[0][3])} \n----------------------------\n {colorir(matriz[1][0])} | {colorir(matriz[1][1])} | {colorir(matriz[1][2])} | {colorir(matriz[1][3])} \n----------------------------\n {colorir(matriz[2][0])} | {colorir(matriz[2][1])} | {colorir(matriz[2][2])} | {colorir(matriz[2][3])} \n----------------------------\n {colorir(matriz[3][0])} | {colorir(matriz[3][1])} | {colorir(matriz[3][2])} | {colorir(matriz[3][3])} \n----------------------------\n')
 
-
 tabuleiro = [
     ['    ', '    ', '    ', '    '],
     ['    ', '    ', '    ', '    '],
     ['    ', '    ', '    ', '    '],
     ['    ', '    ', '    ', '    ']
     ]
+jogador = 1
 
 while True:
-
+    
     while True:
+        #jogador 1 escolhe a peça para o jogador 2 jogar
+        print('As peças possuem as seguintes características:\n')
+        print('Azul: A\nVermelho: V\nQuadrada: Q\nRedonda: R\nGrande: G\nPequena: P\nFurada: F\nNão furada: N\n\n')
+        print('Exemplo: AQRG\n')
+        print('A peça AQRG é uma peça azul, quadrada, grande e furada\n')
         peca = input('Qual peça escolhe pro seu adversário? ')
         print('\n')
-        if 'A' in peca or 'V' in peca:
+        if ('A' in peca or 'V' in peca) and ('Q' in peca or 'R' in peca) and ('G' in peca or 'P' in peca) and ('F' in peca or 'N' in peca):
             break
         else:
             print('Peça inválida\n')
 
+    print(f'Jogador {jogador} escolheu a peça {peca} para o outro jogador jogar!\n')
+    if jogador == 2:
+        jogador = 1
+    elif jogador == 1:
+        jogador = 2
+    
+    
     while True:
-        x = int(input('Digite a linha: '))
-        y = int(input('Digite a coluna: '))
+        x = int(input('Digite a linha que deseja colocar a peça: '))
+        y = int(input('Digite a coluna que deseja colocar a peça: '))
         if x > 3 or x < 0 or y > 3 or y < 0 or tabuleiro[x][y] != '    ':
             print('Posição inválida\n')
         else:
@@ -53,8 +65,9 @@ while True:
         print('Deu velha!')
         break
     elif verificar(tabuleiro) != True:
-        print(f'{verificar(tabuleiro)} venceu!')
+        print(f'{jogador} venceu juntando 4 peças com a característica {verificar(tabuleiro)}!')
         break
-        
-    
-    
+
+
+
+print('\nFim de jogo. Obrigado por jogar!\n')
